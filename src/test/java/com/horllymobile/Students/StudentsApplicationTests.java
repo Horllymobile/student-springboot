251fe2ea82d5ed9fcc09bb22dabfafa267f30b5e
Abraham Olamide James <horlamidex1@gmail.com>
2021-03-15 10:31:37 +0100

    First initial commit

---
+++ src/test/java/com/horllymobile/Students/StudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horllymobile.Students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
